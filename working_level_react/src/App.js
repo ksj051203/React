ed4d6280e6e15f4d82556fa1ba76fd6ac4e497d5
ksj051203 <ksj054703@gmail.com>
2022-07-17 19:32:11 +0900

    render() & constructor

---
+++ working_level_react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LifecycleEx from "./R005_LifecycleEx"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Start React 200!</h1>
+      <p>HTML 적용하기</p>
+      <LifecycleEx />
+    </div>
+  );
+}
+
+export default App;
