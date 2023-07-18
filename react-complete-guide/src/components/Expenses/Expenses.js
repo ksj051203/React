95665d4debbd60f3f89250d5819a9325bb24525d
ksj051203 <ksj054703@gmail.com>
2023-07-18 15:38:12 +0900

    FEAT :: style, rendering

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from '../NewExpense/ExpensesFilter';
 import ExpensesList from './ExpensesList';
 import { useState } from 'react';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
 
@@ -15,10 +16,12 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     })
+    console.log(filteredExpenses)
 
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            <ExpensesChart expenses = {filteredExpenses}/>
             <ExpensesList items = {filteredExpenses} />
         </Card> 
     )
