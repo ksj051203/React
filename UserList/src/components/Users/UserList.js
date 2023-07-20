b6ec7a80e67e389ba8b43c6e6fbf78a4152da336
ksj051203 <ksj054703@gmail.com>
2023-07-20 22:07:08 +0900

    FEAT :: modal 창 구현

--- UserList/src/components/Users/UserList.js
+++ UserList/src/components/Users/UserList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './UserList.module.css';
 import Card from '../UI/Card';
+
 const UserList = (props) => {
     return(
         <Card className={classes.users}>
