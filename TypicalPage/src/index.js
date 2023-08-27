92163f6173a469d95b12014f211fd5506b5cf452
ksj051203 <ksj054703@gmail.com>
2023-08-27 22:42:14 +0900

    FEAT :: Email Validate

--- TypicalPage/src/index.js
+++ TypicalPage/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import Reducer from './components/Example/useReducer/School';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Reducer />);
+root.render(<App />);
